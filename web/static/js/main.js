f4ed076cfd7f54dde1284168e7aafed857d51fd4
Magnhild <magsletten@gmail.com>
2017-12-03 16:22:55 +0100

    Implemented paging in web app

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -3,24 +3,36 @@ traitRandomiserWebApp.navigation = {};
 
 // Define a constant for the active navigation element class name
 const ACTIVE = "active";
+const HIDDEN = "hidden";
 
 // Define and initialise the navigation elements
 var navHome = document.getElementById("navbar_home");
 var navGenerate = document.getElementById("navbar_generate");
 var navAbout = document.getElementById("navbar_about");
 
+// Define and initialise the content divs
+var divHome = document.getElementById("home");
+var divGenerate = document.getElementById("generate");
+var divAbout = document.getElementById("about");
+
 // Pass a new function to the onclick of each navigation element
 navHome.onclick = function(){
 
   traitRandomiserWebApp.navigation.selectNavbarItem(navHome);
+
+  traitRandomiserWebApp.navigation.selectDiv(divHome);
 }
 navGenerate.onclick = function(){
 
   traitRandomiserWebApp.navigation.selectNavbarItem(navGenerate);
+
+  traitRandomiserWebApp.navigation.selectDiv(divGenerate);
 }
 navAbout.onclick = function(){
 
   traitRandomiserWebApp.navigation.selectNavbarItem(navAbout);
+
+  traitRandomiserWebApp.navigation.selectDiv(divAbout);
 }
 
 // Helper functions
@@ -40,4 +52,22 @@ traitRandomiserWebApp.navigation.selectNavbarItem = function(navbarItem){
 
   // Select the passed item
   navbarItem.classList.add(ACTIVE);
+};
+
+traitRandomiserWebApp.navigation.hideAllDivs = function(){
+
+  // Retrieve all divs and hide them
+  var divs = document.getElementsByTagName("div");
+
+  for (var i = 0; i < divs.length; i++){
+
+    divs[i].classList.add(HIDDEN);
+  };
+};
+traitRandomiserWebApp.navigation.selectDiv = function(div){
+
+  traitRandomiserWebApp.navigation.hideAllDivs();
+
+  // Show the passed div
+  div.classList.remove(HIDDEN);
 };
