ff682c430f82510687e4bd3d8c14c9d08aecdfef
Callum Veale <callum.veale@gmail.com>
2017-12-10 01:44:28 +0100

    Updated library and now handle various synonyms for nature in REST service Alexa endpoint.

--- rest_service/src/main/java/com/variableclass/traiter/Controller.java
+++ rest_service/src/main/java/com/variableclass/traiter/Controller.java
@@ -4,6 +4,9 @@ import com.google.gson.*;
 import com.variableclass.traiter.models.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 @RestController
 public class Controller {
 
@@ -57,23 +60,68 @@ public class Controller {
   }
   
   private Nature getNatureFromJson(JsonObject json, TraitRandomiser randomiser) {
-	  
+
+    String natureLabel = null;
+
 	  try {
 		    
 	    // Retrieve nature label from JSON request body
-	    JsonObject jsonIntent = json.get("intent").getAsJsonObject();
+      JsonObject jsonRequest = json.get("request").getAsJsonObject();
+      JsonObject jsonIntent = jsonRequest.get("intent").getAsJsonObject();
 	    JsonObject jsonSlots = jsonIntent.get("slots").getAsJsonObject();
 	    JsonObject jsonNature = jsonSlots.get("Nature").getAsJsonObject();
-	    String natureLabel = jsonNature.get("value").getAsString();
-	    
-	    // Retrieve nature of Alexa-provided label
-	    return randomiser.getNatureFromLabel(natureLabel);
+	    natureLabel = jsonNature.get("value").getAsString().toUpperCase();
 	    
     } catch (NullPointerException e) {
     	
     }
-	  
-	  return null;
+
+    ArrayList<String> positiveSynonyms = new ArrayList<>();
+    positiveSynonyms.add("GOOD");
+    positiveSynonyms.add("HAPPY");
+    positiveSynonyms.add("FAVOURABLE");
+    positiveSynonyms.add("NICE");
+    positiveSynonyms.add("A+");
+    positiveSynonyms.add("ACE");
+    positiveSynonyms.add("BRILLIANT");
+    positiveSynonyms.add("WONDERFUL");
+    positiveSynonyms.add("GREAT");
+    positiveSynonyms.add("MARVELOUS");
+    positiveSynonyms.add("LOVELY");
+    positiveSynonyms.add("AMAZING");
+
+    ArrayList<String> neutralSynonyms = new ArrayList<>();
+    neutralSynonyms.add("NORMAL");
+    neutralSynonyms.add("STANDARD");
+    neutralSynonyms.add("BASIC");
+    neutralSynonyms.add("VANILLA");
+
+    ArrayList<String> negativeSynonyms = new ArrayList<>();
+    negativeSynonyms.add("BAD");
+    negativeSynonyms.add("SHIT");
+    negativeSynonyms.add("RUBBISH");
+    negativeSynonyms.add("CRAP");
+    negativeSynonyms.add("NASTY");
+    negativeSynonyms.add("AWFUL");
+    negativeSynonyms.add("LAME");
+    negativeSynonyms.add("LOUSY");
+    negativeSynonyms.add("NAFF");
+
+    if (positiveSynonyms.contains(natureLabel)){
+
+      natureLabel = "POSITIVE";
+
+    } else if (neutralSynonyms.contains(natureLabel)){
+
+      natureLabel = "NEUTRAL";
+
+    } else if (negativeSynonyms.contains(natureLabel)){
+
+      natureLabel = "NEGATIVE";
+    }
+
+    // Retrieve nature of Alexa-provided label
+    return randomiser.getNatureFromLabel(natureLabel);
   }
   
   private Category getCategoryFromJson(JsonObject json, TraitRandomiser randomiser) {
@@ -81,10 +129,11 @@ public class Controller {
   	try {
       
 	    // Retrieve category label from JSON request body
-	    JsonObject jsonIntent = json.get("intent").getAsJsonObject();
+      JsonObject jsonRequest = json.get("request").getAsJsonObject();
+	    JsonObject jsonIntent = jsonRequest.get("intent").getAsJsonObject();
 	    JsonObject jsonSlots = jsonIntent.get("slots").getAsJsonObject();
 	    JsonObject jsonCategory = jsonSlots.get("Category").getAsJsonObject();
-	    String categoryLabel = jsonCategory.get("value").getAsString();
+	    String categoryLabel = jsonCategory.get("value").getAsString().toUpperCase();
 	    
 	    // Retrieve category of Alexa-provided label
 	    return randomiser.getCategoryFromLabel(categoryLabel);
